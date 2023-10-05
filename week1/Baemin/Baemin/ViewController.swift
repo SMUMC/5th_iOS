f1d1d49c31dbc7a34c60fe75ae9697a6a47453a7
LeeHyeonHo-127 <swethom1@naver.com>
2023-10-05 13:21:04 +0900

    [Add] Week 1

---
+++ week1/Baemin/Baemin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Baemin
+//
+//  Created by 이현호 on 2023/10/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
